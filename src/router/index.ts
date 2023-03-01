3fb22db8dbc3ac127ab9171366fb51755ba98d21
剧终 <13824303245@163.com>
2023-03-01 11:18:57 +0800

    1. 修复路由打包后部署白屏问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,28 +8,28 @@ const componentMatch = import.meta.glob('../views/**/index.vue')
 
 const routes: RouteRecordRaw[] = []
 const dynamicRoutes = Object.entries(pagesMatches).map(([pagePath, config]) => {
-	const path = pagePath.replace(/(..\/views|\/components|\/page.ts)/g, '') || '/'
-	const name =
-		path
-			.split('/')
-			.filter(Boolean)
-			.filter((x) => x !== 'components')
-			.join('-') || 'index'
-	const componentPath = pagePath.replace('page.ts', 'index.vue')
-	return {
-		path,
-		name,
-		meta: config || {},
-		component: componentMatch[componentPath]
-	} as RouteRecordRaw
+  const path = pagePath.replace(/(..\/views|\/components|\/page.ts)/g, '') || '/'
+  const name =
+    path
+      .split('/')
+      .filter(Boolean)
+      .filter((x) => x !== 'components')
+      .join('-') || 'index'
+  const componentPath = pagePath.replace('page.ts', 'index.vue')
+  return {
+    path,
+    name,
+    meta: config || {},
+    component: componentMatch[componentPath]
+  } as RouteRecordRaw
 })
 
 const x = buildRouterTree(dynamicRoutes)
 if (!x.find((x) => x.path === '/')) {
-	routes.push({ path: '/', name: 'index', redirect: dynamicRoutes[0].path, component: Layout, children: x })
+  routes.push({ path: '/', name: 'index', redirect: dynamicRoutes[0].path, component: Layout, children: x })
 }
 
 export default createRouter({
-	history: createWebHistory(),
-	routes: routes
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: routes
 })
